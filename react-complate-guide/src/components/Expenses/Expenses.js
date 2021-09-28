25a9f2fef1ff8ff933f52a371440ff66176d2562
enes_guner <enes@gulenayva.com>
2021-09-28 13:40:58 +0300

    filter drowdown

--- react-complate-guide/src/components/Expenses/Expenses.js
+++ react-complate-guide/src/components/Expenses/Expenses.js
@@ -1,3 +1,4 @@
+import ExpensesFilter from "./ExpenseFilter";
 import ExpenseItem from "./Expenseltem";
 
 
@@ -6,12 +7,17 @@ function Expenses(props){
     
 
     return(
-        <div className="expenses" >
-            <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
-            <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
-            <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} />
-            <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date} />
-            <ExpenseItem title={props.items[4].title} amount={props.items[4].amount} date={props.items[4].date} />
+        <div>
+            
+        
+            <div className="expenses" >
+                <ExpensesFilter/>
+                <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
+                <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
+                <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} />
+                <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date} />
+                <ExpenseItem title={props.items[4].title} amount={props.items[4].amount} date={props.items[4].date} />
+            </div>
         </div>
     )
 }
